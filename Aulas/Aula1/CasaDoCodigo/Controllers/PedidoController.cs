c3a9d294e61e62fc6743b2d46e4c5082b0e112e7
maanoel <manoelvitorbrito@gmail.com>
2021-12-01 00:01:33 -0300

    criando carrosel

---
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CasaDoCodigo.Controllers
+{
+  public class PedidoController: Controller
+  {
+    public IActionResult Carrossel() 
+    {
+      return View();
+    }
+  }
+}
