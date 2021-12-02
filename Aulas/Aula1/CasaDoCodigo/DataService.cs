f7786ea7e99d94872615360eb3540f19aa460b80
maanoel <manoelvitorbrito@gmail.com>
2021-12-01 21:13:18 -0300

    deserializando objetos

--- Aulas/Aula1/CasaDoCodigo/DataService.cs
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CasaDoCodigo
 {
@@ -15,6 +18,18 @@ namespace CasaDoCodigo
     public void InicializaDB()
     {
       contexto.Database.Migrate();
+
+      var json = File.ReadAllText("livros.json");
+      var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
     }
+
+    class Livro
+    {
+      public string Codigo { get; set; }
+      public string Nome { get; set; }
+      public decimal Preco { get; set; }
+    }
+
+
   }
 }
