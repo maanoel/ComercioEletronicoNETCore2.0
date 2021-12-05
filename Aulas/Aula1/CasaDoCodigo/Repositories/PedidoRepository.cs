6fbf4c30da8b21f1e77078f23a920e63c63c6668
maanoel <manoelvitorbrito@gmail.com>
2021-12-05 18:02:21 -0300

    Include e ThenInclude

--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CasaDoCodigo.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Linq;
 
@@ -17,6 +18,8 @@ namespace CasaDoCodigo
     {
       var pedidoId = ObterPedidoId();
       var pedido = dbSet
+      .Include(p=> p.Itens)
+      .ThenInclude(i=>i.Produto)
       .Where(p => p.Id == pedidoId)
       .SingleOrDefault();
 
