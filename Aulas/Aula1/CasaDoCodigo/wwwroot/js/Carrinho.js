736063ac51d2b727b7f67e674721d13637c19e84
maanoel <manoelvitorbrito@gmail.com>
2021-12-05 23:44:53 -0300

    Criando l¾gica no javascript para incrementar

---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/Carrinho.js
@@ -0,0 +1,36 @@
+﻿class Carrinho {
+    clickIncremento(btn) {
+        let data = this.obterData(btn);
+        data.Quantidade++;
+        this.postarQuantidade(data);
+    }
+
+    clickDecremento(btn) {
+        let data = this.obterData(btn);
+        data.Quantidade--;
+        this.postarQuantidade(data);
+    }
+
+    postarQuantidade(data) {
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(data)
+        })
+    }
+
+    obterData(elemento) {
+        let linhaDoItem = $(elemento).parents('[item-id]');
+        let itemId = $(linhaDoItem).attr('item-id');
+        let novaQtde = $(linhaDoItem).find('input').val();
+
+        return data = {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+    }
+}
+
+var carrinho = new Carrinho();
+
